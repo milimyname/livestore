00b3518afe4258ccddc12e041f76b777c6293674
mili <kamil.maksudov2019@gmail.com>
2025-07-13 01:52:26 +0200

    capacitor livereload

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,25 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.packsil.com',
-  appName: 'packsil',
-  webDir: 'dist'
+	appId: 'com.packsil.com',
+	appName: 'packsil',
+	webDir: 'build',
+	server: {
+		url: 'http://172.20.10.8:5173/', // !IMPORTANT: remove on deployment
+		cleartext: true,
+		androidScheme: 'https'
+	},
+	plugins: {
+		SplashScreen: {
+			launchShowDuration: 2000,
+			backgroundColor: '#34495e',
+			showSpinner: false
+		},
+		StatusBar: {
+			backgroundColor: '#34495e',
+			style: 'light'
+		}
+	}
 };
 
 export default config;
